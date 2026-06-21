6ca56efe78e7aedbf1927ab711309d779508253c
Rennan Dutra <rennandutraa@gmail.com>
2026-06-21 20:22:48 -0300

    corrigir chave supabase

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://vheeretqedhgfmlrexiw.supabase.co/rest/v1/";
-const supabaseAnonKey = "SUA-ANON-KEY-REAL";
+const supabaseUrl = "https://https://vheeretqedhgfmlrexiw.supabase.co";
+const supabaseAnonKey = "sb_publishable_LXFChzHs-jXX5Xcm7E5S8A_g2o8CFL_";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
