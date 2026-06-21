af00f31c9361c51815fe6683d3c6e7ca7bf46605
Rennan Dutra <rennandutraa@gmail.com>
2026-06-21 20:16:50 -0300

    corrigir url supabase

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://https://vheeretqedhgfmlrexiw.supabase.co/rest/v1/";
+const supabaseUrl = "https://vheeretqedhgfmlrexiw.supabase.co/rest/v1/";
 const supabaseAnonKey = "SUA-ANON-KEY-REAL";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
